5f4c6730bd1220a180320342bb8b0b565fe4317a
gdsabarna353 <gdsabarna353@gmail.com>
2026-01-17 21:31:54 +0530

    Task 2 completed

--- app.py
+++ app.py
@@ -4,16 +4,10 @@ app = Flask(__name__)
 
 tasks = []
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World guys!</p>"
-
 @app.route("/health", methods=['GET'])
 def get_health():
     if(request.method == 'GET'):
-        return {
-            "status": "ok"
-        }
+        return jsonify({"status": "ok"}), 200
 
 @app.route('/tasks', methods=['GET', 'POST'])
 def post_tasks():
@@ -29,6 +23,27 @@ def post_tasks():
         tasks.append(new_task)
         return jsonify(new_task), 201
     else:
-        return tasks, 200
+        return jsonify(tasks), 200
+
+@app.route("/tasks/<int:id>", methods=['PUT', 'DELETE'])
+def updateAndDelete_tasks(id):
+    # global tasks
+    task_found = False
+    if(request.method == 'PUT'):
+        for task in tasks:
+            if(task['id'] == id):
+                task_found = True
+                task['completed'] = True
+                return jsonify(task), 200
+    elif(request.method == 'DELETE'):
+        for task in tasks:
+            if(task['id'] == id):
+                # newtasks = filter(lambda task: task['id'] != int(id), tasks)
+                # tasks = list(newtasks)
+                tasks.remove(task)
+                # tasks.pop(int(id)-1)
+                return jsonify({"message": "Task deleted successfully"}), 200
+    if(task_found == False):
+        return jsonify({"error": "Task not found"}), 404
 
 app.run(debug=True)
